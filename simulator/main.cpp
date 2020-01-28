1952c0875cb41f6417932f68fc1d7f6029636399
Andrew Petelin <adrianopol@gmail.com>
2020-01-28 15:12:54 +0300

    CYB-518 Fix simulator for full nodes.

--- simulator/main.cpp
+++ simulator/main.cpp
@@ -1,13 +1,24 @@
-#include <gtest/gtest.h>
 #include "log.hpp"
+
 #include <fc/log/logger.hpp>
+#include <fc/log/logger_config.hpp>
+
+#include <gtest/gtest.h>
 
 using namespace std;
+
+void init_randpa_logger() {
+    auto cfg = fc::logging_config::default_config();
+    cfg.loggers.push_back(cfg.loggers[0]);
+    cfg.loggers.back().name = "randpa_plugin";
+    fc::log_config::configure_logging(cfg);
+}
 
 int main(int argc, char **argv) {
     auto seed = time(NULL);
     std::srand(seed);
     std::cout << "Random number generator seeded to " << seed << std::endl;
+    init_randpa_logger();
 
     bool is_verbose = false;
     std::string verbose_arg = "--verbose";
@@ -18,7 +29,9 @@ int main(int argc, char **argv) {
        }
     }
 
-    if (!is_verbose) {
+    if (is_verbose) {
+        fc::logger::get().set_log_level(fc::log_level::debug);
+    } else {
         std::cout << "Logging disabled, use --verbose to enable" << std::endl;
         logger.enabled(false);
         fc::logger::get().set_log_level(fc::log_level::off);
